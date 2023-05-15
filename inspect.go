ca685bd1637d80417e0dc359277a22eb2a15b4f9
a0171610 <kohei-okazaki@c-fo.com>
2023-05-16 08:22:50 +0900

    :sparkles: add inspect file for generate code

---
+++ inspect.go
@@ -0,0 +1,58 @@
+package ayaorm
+
+// ref: https://tech.buysell-technologies.com/entry/adventcalendar2022-12-06
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+)
+
+func Inspect(path string) map[string]string {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, path, nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fields := make(map[string]string)
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch n.(type) {
+		case *ast.TypeSpec:
+			s, _ := n.(*ast.TypeSpec)
+			v, ok := s.Type.(*ast.StructType)
+			if !ok {
+				return false
+			}
+			// 構造体かつその名前が対象のモデルの場合
+			for _, l := range v.Fields.List {
+				if len(l.Names) <= 0 {
+					continue
+				}
+				switch l.Type.(type) {
+				case *ast.Ident: // intやstringのようなプリミティブな型の場合
+					t, _ := l.Type.(*ast.Ident)
+					fields[l.Names[0].Name] = t.Name
+				case *ast.SelectorExpr: // time.Timeやnull.Stringのような型
+					t, _ := l.Type.(*ast.SelectorExpr)
+					x, _ := t.X.(*ast.Ident)
+					name := x.Name + "." + t.Sel.Name
+					fields[l.Names[0].Name] = name
+				case *ast.StarExpr:
+					t, _ := l.Type.(*ast.StarExpr)
+					switch t.X.(type) {
+					case *ast.Ident: // *intのようなプリミティブのポインタ型
+					// 処理内容は上のIdentとほぼ同じなので省略
+					case *ast.SelectorExpr: // *time.Timeのようなポインタ型
+						// 処理内容は上のSelectorExprとほぼ同じなので省略
+					}
+				}
+			}
+		}
+		return true
+	})
+
+	return fields
+}
