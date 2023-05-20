29e8268645c63b263d0b4819915b174957896f00
岡崎 <61766487+okazaki-kk@users.noreply.github.com>
2023-05-21 08:39:12 +0900

    Add generate command (#32)
    * :+1: add generate command
    
    * add white new line

--- cmd/ayaorm/main.go
+++ cmd/ayaorm/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strings"
+
 	"github.com/okazaki-kk/ayaorm"
 	"github.com/okazaki-kk/ayaorm/template"
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("please specify generate file")
+	}
+	from := os.Args[1]
+	filePath := strings.Split(from, ".go")[0] + "_gen.go"
+
 	modelName, fieldKeys, _ := ayaorm.Inspect("./example/user.go")
-	filePath := "./main_gen.go"
 	template.Generate(filePath, modelName, fieldKeys)
 }
