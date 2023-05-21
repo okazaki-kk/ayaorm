856fce6573a68a12e4f029355d253914490e2f0e
okazaki-kk <ozaki.m.mi.ys@gmail.com>
2023-05-21 09:29:24 +0900

    :+1: add readme

--- cmd/ayaorm/main.go
+++ cmd/ayaorm/main.go
@@ -16,6 +16,6 @@ func main() {
 	from := os.Args[1]
 	filePath := strings.Split(from, ".go")[0] + "_gen.go"
 
-	modelName, fieldKeys, _ := ayaorm.Inspect("./example/user.go")
+	modelName, fieldKeys, _ := ayaorm.Inspect(from)
 	template.Generate(filePath, modelName, fieldKeys)
 }
