11729e1071b4cd4bf2d1dcb147b265d52ff1ba7b
okazaki-kk <ozaki.m.mi.ys@gmail.com>
2023-05-21 17:32:32 +0900

    :+1: add experimental inner join

--- example_experimental/comment_post_gen.go
+++ example_experimental/comment_post_gen.go
@@ -7,3 +7,21 @@ func (m Post) Comments() ([]*Comment, error) {
 	}
 	return comments, nil
 }
+
+func (m Post) JoinComments() ([]*Post, error) {
+	var posts []*Post
+	rows, err := db.Query(`
+		select posts.id, posts.content, posts.author, posts.created_at, posts.updated_at from posts
+			inner join comments
+			on posts.id = comments.post_id;
+	`)
+	for rows.Next() {
+		var post Post
+		err := rows.Scan(&post.Id, &post.Content, &post.Author, &post.CreatedAt, &post.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, &post)
+	}
+	return posts, err
+}
