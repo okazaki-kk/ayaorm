c51b6dc51fcf86428fc484a865ca036261362234
a0171610 <kohei-okazaki@c-fo.com>
2023-05-25 22:48:51 +0900

    :shower:

--- example_experimental/comment_post.go
+++
@@ -1,4 +0,0 @@
-package main
-
-func (m Comment) hasManyPosts() {
-}
--- example_experimental/post.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/okazaki-kk/ayaorm"
-
-type Post struct {
-	ayaorm.Schema
-	Content string
-	Author  string
-}
